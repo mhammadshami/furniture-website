c1dad0f33757cd9a189356204e6472563ca0dbe5
Mohammad Alshami <elshami312@gmail.com>
2023-08-07 22:48:10 +0300

    first commit

---
+++ src/components/Programs.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+const Programs = () => {
+  return (
+    <section className="bg-[#f9f9f9] py-10 mb-11">
+      <div className="container mx-auto">
+        <h2 className="w-fit text-3xl relative font-semibold mx-auto uppercase after:content-[''] after:absolute after:bg-[#009688] after:h-[2px] after:w-[40%] after:left-[50%] after:translate-x-[-50%] after:-bottom-3">
+          Ghi Programs
+        </h2>
+        <div className="flex items-center justify-center flex-wrap gap-[30px] mt-20">
+          <div className="relative w-[270px] top-0 group bg-white text-center pb-11 transition-all duration-[400ms] shadow-lg shadow-gray-300 cursor-pointer rounded-xl overflow-hidden hover:top-[-30px] after:content-[''] after:absolute after:h-[4px] after:w-0 after:top-0 after:left-2/4 after:bg-[#009688] after:translate-x-[-50%] after:transition-all after:duration-[200ms] hover:after:w-full">
+            <div className="relative after:content-[''] after:transition-all after:absolute after:bottom-0 after:right-0 after:border-solid after:border-t-0 after:border-r-0 after:border-b-[48px] after:border-l-[309px] after:border-t-transparent after:border-t-transparent after:border-r-transparent after:border-b-white after:border-l-transparent group-hover:after:border-[48px_309px_48px_0] overflow-hidden">
+              <img
+                className="w-full object-contain"
+                src="https://media-ghi.ghi.aub.edu.lb/wp-content/uploads/2023/01/05093230/esp.png"
+                alt=""
+              />
+            </div>
+            <h3 className="font-semibold my-4 text-2xl">Esp</h3>
+            <p className="text-gray-500 mb-4">E-Sahha Program</p>
+            <button className="mt-[30px] border-[3px] border-[#009688] text-[#009688] p-[10px_30px] transition-all font-semibold bg-gradient-to-r from-[#009688_50%] to-[#FFF_50%] bg-[right_bottom] group-hover:bg-[left_bottom] group-hover:text-white bg-[length:200%_100%]">
+              Learn more
+            </button>
+          </div>
+          <div className="relative w-[270px] top-0 group bg-white text-center pb-11 transition-all duration-[400ms] shadow-lg shadow-gray-300 cursor-pointer rounded-xl overflow-hidden hover:top-[-30px] after:content-[''] after:absolute after:h-[4px] after:w-0 after:top-0 after:left-2/4 after:bg-[#009688] after:translate-x-[-50%] after:transition-all after:duration-[200ms] hover:after:w-full">
+            <div className="relative after:content-[''] after:transition-all after:absolute after:bottom-0 after:right-0 after:border-solid after:border-t-0 after:border-r-0 after:border-b-[48px] after:border-l-[309px] after:border-t-transparent after:border-t-transparent after:border-r-transparent after:border-b-white after:border-l-transparent group-hover:after:border-[48px_309px_48px_0] overflow-hidden">
+              <img
+                className="w-full object-contain"
+                src="https://media-ghi.ghi.aub.edu.lb/wp-content/uploads/2023/01/05093230/esp.png"
+                alt=""
+              />
+            </div>
+            <h3 className="font-semibold my-4 text-2xl">Esp</h3>
+            <p className="text-gray-500 mb-4">E-Sahha Program</p>
+            <button className="mt-[30px] border-[3px] border-[#009688] text-[#009688] p-[10px_30px] transition-all font-semibold bg-gradient-to-r from-[#009688_50%] to-[#FFF_50%] bg-[right_bottom] group-hover:bg-[left_bottom] group-hover:text-white bg-[length:200%_100%]">
+              Learn more
+            </button>
+          </div>
+          <div className="relative w-[270px] top-0 group bg-white text-center pb-11 transition-all duration-[400ms] shadow-lg shadow-gray-300 cursor-pointer rounded-xl overflow-hidden hover:top-[-30px] after:content-[''] after:absolute after:h-[4px] after:w-0 after:top-0 after:left-2/4 after:bg-[#009688] after:translate-x-[-50%] after:transition-all after:duration-[200ms] hover:after:w-full">
+            <div className="relative after:content-[''] after:transition-all after:absolute after:bottom-0 after:right-0 after:border-solid after:border-t-0 after:border-r-0 after:border-b-[48px] after:border-l-[309px] after:border-t-transparent after:border-t-transparent after:border-r-transparent after:border-b-white after:border-l-transparent group-hover:after:border-[48px_309px_48px_0] overflow-hidden">
+              <img
+                className="w-full object-contain"
+                src="https://media-ghi.ghi.aub.edu.lb/wp-content/uploads/2023/01/05093230/esp.png"
+                alt=""
+              />
+            </div>
+            <h3 className="font-semibold my-4 text-2xl">Esp</h3>
+            <p className="text-gray-500 mb-4">E-Sahha Program</p>
+            <button className="mt-[30px] border-[3px] border-[#009688] text-[#009688] p-[10px_30px] transition-all font-semibold bg-gradient-to-r from-[#009688_50%] to-[#FFF_50%] bg-[right_bottom] group-hover:bg-[left_bottom] group-hover:text-white bg-[length:200%_100%]">
+              Learn more
+            </button>
+          </div>
+          <div className="relative w-[270px] top-0 group bg-white text-center pb-11 transition-all duration-[400ms] shadow-lg shadow-gray-300 cursor-pointer rounded-xl overflow-hidden hover:top-[-30px] after:content-[''] after:absolute after:h-[4px] after:w-0 after:top-0 after:left-2/4 after:bg-[#009688] after:translate-x-[-50%] after:transition-all after:duration-[200ms] hover:after:w-full">
+            <div className="relative after:content-[''] after:transition-all after:absolute after:bottom-0 after:right-0 after:border-solid after:border-t-0 after:border-r-0 after:border-b-[48px] after:border-l-[309px] after:border-t-transparent after:border-t-transparent after:border-r-transparent after:border-b-white after:border-l-transparent group-hover:after:border-[48px_309px_48px_0] overflow-hidden">
+              <img
+                className="w-full object-contain"
+                src="https://media-ghi.ghi.aub.edu.lb/wp-content/uploads/2023/01/05093230/esp.png"
+                alt=""
+              />
+            </div>
+            <h3 className="font-semibold my-4 text-2xl">Esp</h3>
+            <p className="text-gray-500 mb-4">E-Sahha Program</p>
+            <button className="mt-[30px] border-[3px] border-[#009688] text-[#009688] p-[10px_30px] transition-all font-semibold bg-gradient-to-r from-[#009688_50%] to-[#FFF_50%] bg-[right_bottom] group-hover:bg-[left_bottom] group-hover:text-white bg-[length:200%_100%]">
+              Learn more
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Programs;
